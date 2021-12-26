ff9d76ebd4d684bd72ec64bb532ae1522fa0ff31
KaiserRuben <ruben2412000@gmail.com>
2021-12-26 12:56:35 +0100

    finished first section

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,25 @@
   <router-view/>
 </template>
 
-<style>
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Petemoss&display=swap')
+body
+  margin: 0
+  background: #0C0D08
+  color: #EDF0F3
+  font-family: Montserrat, sans-serif
+
+h1
+  font-family: Montserrat, sans-serif
+  font-weight: 700
+  letter-spacing: 5px
+  font-size: 60px
+  margin: 10px auto
+
+h2
+  font-family: Petemoss, serif
+
+p
+  font-family: Montserrat, sans-serif
+  margin: 10px auto
 </style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,12 +1,95 @@
 <template>
+  <div>
+
+    <!-- Section 1 (IMG Layout) -->
+    <div class="section imgContainer">
+      <div class="imgGrid1">
+        <div class="imgGrid1_1">
+
+          <div class="imgGrid1_1_1">
+
+          </div>
+          <div class="imgGrid1_1_2">
+
+          </div>
+        </div>
+        <div class="imgGrid1_2">
+
+        </div>
+      </div>
+      <div class="imgGrid2">
+
+      </div>
+    </div>
+    <div class="section">
+      <div class="welcomeSection">
+        <div>
+          <p class="light" style="margin-bottom: 0">
+            Herzlich Willkommen
+          </p>
+          <h1>
+            TUMAINI
+          </h1>
+        </div>
+        <p>
+          test123
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-  },
+  components: {},
 });
 </script>
+<style scoped lang="sass">
+.section
+  height: 100vh
+
+.light
+  font-weight: 300
+
+/* Image Layout for Landing */
+.imgContainer
+  display: grid
+  grid-template-columns: 50vw 50vw
+
+  .imgGrid1
+    display: grid
+    grid-template-rows: 50vh 50vh
+
+    .imgGrid1_1
+      display: grid
+      grid-template-columns: 33% 67%
+
+      .imgGrid1_1_1
+        background: url("../assets/landing/annie-spratt-0cgpyigyIkM-unsplash.webp") no-repeat center
+        background-size: auto 120%
+
+      .imgGrid1_1_2
+        background: url("../assets/landing/annie-spratt-cVEOh_JJmEE-unsplash.webp") no-repeat center
+        background-size: auto 120%
+
+    .imgGrid1_2
+      background: url("../assets/landing/ben-hummitzsch-pYTgvmpuQWs-unsplash.webp") no-repeat center
+      background-size: 120% auto
+
+  .imgGrid2
+    background: url("../assets/landing/carolinie-cavalli-yFaK9jgQeb4-unsplash.webp") no-repeat center
+    background-size: auto 120%
+
+.welcomeSection
+  display: flex
+  flex-direction: column
+  flex-wrap: nowrap
+  align-items: center
+  justify-content: center
+  text-align: center
+
+  height: 100%
+</style>
