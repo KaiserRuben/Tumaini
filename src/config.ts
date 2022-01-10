db86068621d341536a23761e78b95a21fca11c3b
KaiserRuben <ruben2412000@gmail.com>
2022-01-10 22:49:51 +0100

    Set deployment to prod

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV === "development"
-const dbOnline = false
+const dbOnline = true
 
 export const SERVER_ADDRESS = dev && !dbOnline ? "http://localhost:3000" : "https://api.tumaini.be"
