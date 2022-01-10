5a3a0d66f90058df27a8f8569f69a1f628c71b78
KaiserRuben <ruben2412000@gmail.com>
2022-01-10 22:49:51 +0100

    Set deployment to prod

--- admin/src/config.ts
+++ admin/src/config.ts
@@ -1,4 +1,5 @@
-export const SERVER_ADDRESS = 'https://api.tumaini.be';
-// export const SERVER_ADDRESS = 'http://localhost:3000';
+const dbOnline = true
+
+export const SERVER_ADDRESS = !dbOnline ? "http://localhost:3000" : "https://api.tumaini.be"
 export const MEDIA_LOCATION = `https://files.tumaini.be/`
 export const FILES_LOCATION = `https://files.tumaini.be/`
