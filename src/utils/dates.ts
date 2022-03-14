fdc77bab9a9236890ce6cbd3b233d88731573b31
KaiserRuben <ruben2412000@gmail.com>
2022-03-14 20:38:08 +0100

    Improved article sort function

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,5 +1,11 @@
 import {IArticle} from "../../api/models/article";
 
 export function sortArticles(a: IArticle, b: IArticle): number {
-    return (new Date(b.created).getDate()) - (new Date(a.created).getDate())
+    let n = 0
+    try {
+        n = (new Date(b.created).getDate()) - (new Date(a.created).getDate())
+    } catch (e) {
+        console.warn("Problem while sorting articles. Expect limited experience: " + e)
+    }
+    return n
 }
