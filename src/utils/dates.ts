1d5419d2d9baf1a3a059aa1baed1a41a25417220
Ruben Kaiser <Ruben2412000@gmail.com>
2023-02-10 17:04:30 +0100

    Bugfix: Neuester bericht wird jetzt immer auf der Landing angezeigt.

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -3,7 +3,7 @@ import {IArticle} from "../../api/models/article";
 export function sortArticles(a: IArticle, b: IArticle): number {
     let n = 0
     try {
-        n = (new Date(b.created).getDate()) - (new Date(a.created).getDate())
+        n = (new Date(a.created).getDate()) - (new Date(b.created).getDate())
     } catch (e) {
         console.warn("Problem while sorting articles. Expect limited experience: " + e)
     }
